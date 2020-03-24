28a7ed4992587da1749c0c540b8aa686d850745f
xavier <xavierhoskins@outlook.com>
2020-03-23 19:04:58 -0500

    finished my work

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,32 @@
 import React, { useState } from 'react';
-
+import { Route, Link, Switch } from 'react-router-dom';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 import SavedList from './Movies/SavedList';
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+  const [savedList, setSavedList] = useState([]);
 
   const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
+    setSavedList([...savedList, movie]);
   };
 
   return (
     <div>
-      <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <div>
+        <Link to="/">Movie List</Link>
+      </div>
+
+      <div>
+        <SavedList list={savedList} />
+
+        <Route exact path="/">
+          <MovieList />
+        </Route>
+        <Route exact path="/movies/:id">
+          <Movie />
+        </Route>
+      </div>
     </div>
   );
 };
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
-const SavedList = props => (
-  <div className="saved-list">
-    <h3>Saved Movies:</h3>
-    {props.list.map(movie => (
-      <span className="saved-movie">{movie.title}</span>
-    ))}
-    <div className="home-button">Home</div>
-  </div>
-);
+function SavedList(props) {
+  const history = useHistory();
+
+  const routeToShop = () => {
+    history.push('/');
+  };
+
+  return (
+    <div className="saved-list">
+      <h3>Saved Movies:</h3>
+      {props.list.map(movie => (
+        <span className="saved-movie">{movie.title}</span>
+      ))}
+      <div onClick={routeToShop} className="home-button">
+        Home
+      </div>
+    </div>
+  );
+}
 
 export default SavedList;
